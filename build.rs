7c3ad6793c7bc487f08aa7801d263c47eb5da209
Manabu Sugimoto <Manabu.Sugimoto@sony.com>
2021-02-14 16:45:16 +0900

    Apply rustfmt
    Signed-off-by: Manabu Sugimoto <Manabu.Sugimoto@sony.com>

--- build.rs
+++ build.rs
@@ -4,18 +4,13 @@ use std::env;
 fn main() {
     let rustc_link_type = match env::var("LIBSECCOMP_LIB_TYPE") {
         Ok(mode) => mode, // static, framework, dylib
-        Err(_) => String::from("dylib")
+        Err(_) => String::from("dylib"),
     };
 
-    println!(
-        "cargo:rustc-link-lib={}={}",
-        rustc_link_type,
-        "seccomp"
-    );
+    println!("cargo:rustc-link-lib={}={}", rustc_link_type, "seccomp");
 
     match env::var("LIBSECCOMP_LIB_PATH") {
-        Ok(rustc_link_search) =>
-            println!("cargo:rustc-link-search=native={}", rustc_link_search),
+        Ok(rustc_link_search) => println!("cargo:rustc-link-search=native={}", rustc_link_search),
         Err(_) => {}
     };
 
@@ -24,7 +19,6 @@ fn main() {
         .probe("libseccomp")
         .is_ok()
     {
-
         println!("cargo:rustc-cfg=seccomp_notify");
     }
 }
